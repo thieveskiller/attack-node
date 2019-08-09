579030baa56c6a847f343c4d8fecc9fd1dcfc5fd
德莉莎 <yxiao2044@gmail.com>
2019-08-09 21:11:45 +0800

    update
    - 降低内存使用
    - 独立配置文件
    - 新增压测链路

--- app.js
+++ app.js
@@ -1,91 +1,7 @@
 const child_process = require("child_process");
-let processes = new Array();
-let restart = true;
-let n_fail = 0;
-let n_success = 0;
-let n_total = 0;
-let total = {
-    total: 0,
-    success: 0,
-    fail: 0
-};
-let codeList = new Object();
-
-let num = 32;   // 进程数
-let url = "http://yuhailou.cn/mo/yxdl/s1.asp";  // 链接
-let method = "POST";    // 请求方式（POST或GET）
-let t = 10;     // 间隔时间，每个进程单独计算
-let referer = "http://yuhailou.cn/mo/yxdl/xlogin.asp";  //来源
-let data = `username=[QQ]&password=[String_10]`;    // POST发送的数据
-
-logger("INFO", `[INFO][Thread-Main]`, `Starting...`);
-logger("INFO", `[INFO][Thread-Main]`, `Maximum Concurrency: ${(1e3 / t) * num}`);
-
-setInterval(() => {
-    logger("INFO", `[INFO][Thread-Main]`, `total: ${n_total}, fail: ${n_fail}, success: ${n_success}`);
-    n_total = 0;
-    n_fail = 0;
-    n_success = 0;
-}, 1e3);
-
-for (let i = 0; i < num; i++) {
-    processes[i] = child_process.fork('./start.js', {
-        env: {
-            url: url,
-            method: method,
-            t: t,
-            referer: referer,
-            data: data
-        }
-    });
-    processes[i].on('message', (m) => {
-        processEvent.msg(i, m);
-    });
-    processes[i].on('close', (code) => {
-        processEvent.exit(i, code);
-    });
-}
-
-logger("INFO", `[INFO][Thread-Main]`, `Started!`);
-
-var processEvent = {
-    msg: (i, msg) => {
-        let err = msg[0];
-        let code = msg[1];
-        let body = msg[2];
-        total.total++;
-        n_total++;
-        if (isNaN(codeList[String(code)])) {
-            codeList[String(code)] = 1;
-        } else {
-            codeList[String(code)]++;
-        }
-        if (_code(code) && !err) {
-            if (success(body)) {
-                n_success++;
-                total.success++
-            } else {
-                total.fail++
-                logger("WARN", `[WARN][Thread-${i}]`, `Failed`);
-            }
-        } else if (code != null) {
-            n_fail++;
-            total.fail++
-            logger("WARN", `[WARN][Thread-${i}]`, `Code: ${code}`);
-        }
-    },
-    exit: (i, code) => {
-        if (code == 0) {
-            logger("WARN", `[WARN][Thread-${i}]`, `exit ${code}`);
-        } else {
-            logger("ERROR", `[ERROR][Thread-${i}]`, `exit ${code}`);
-        }
-        if (restart) {
-            processes[i] = child_process.fork('./start.js');
-        }
-    }
-}
-
+const fs = require("fs");
+const os = require("os");
+const config = JSON.parse(fs.readFileSync("./config.json").toString());
 
 const logger = (type, title, msg) => {
     let time = new Date();
@@ -96,17 +12,6 @@ const logger = (type, title, msg) => {
     } else if (type == "ERROR") {
         console.error(`\x1b[47m\x1b[34m ${time} \x1b[46m\x1b[31m ${title} \x1b[47m\x1b[32m ${msg} \x1b[0m`);
     }
-}
-
-const success = (data) => {
-    return true;
-    /*
-    if(data.indexOf("") != -1){
-        return true;
-    }else{
-        return false;
-    }
-    */
 }
 
 const _code = (code) => {
@@ -122,6 +27,92 @@ const _code = (code) => {
     }
 }
 
+let processNumber = 0;
+let processes = new Array();
+let restart = true;
+let n_fail = 0;
+let n_success = 0;
+let n_total = 0;
+let total = {
+    total: 0,
+    success: 0,
+    fail: 0
+};
+let codeList = new Object();
+
+if (config.global.processNumber == -1) {
+    processNumber = os.cpus().length * 4;
+} else {
+    processNumber = config.global.processNumber;
+}
+
+logger("INFO", `[INFO][Thread-Main]`, `Powered By 黑与白工作室`);
+logger("INFO", `[INFO][Thread-Main]`, `Starting...`);
+logger("INFO", `[INFO][Thread-Main]`, `Process: ${processNumber}`);
+logger("INFO", `[INFO][Thread-Main]`, `Maximum Concurrency: ${(1e3 / config.global.delay) * processNumber * config.stream.length}`);
+
+setInterval(() => {
+    logger("INFO", `[INFO][Thread-Main]`, `total: ${n_total}, fail: ${n_fail}, success: ${n_success}`);
+    n_total = 0;
+    n_fail = 0;
+    n_success = 0;
+}, 1e3);
+
+for (let i = 0; i < processNumber; i++) {
+    processes[i] = child_process.fork('./start.js', {
+        env: {
+            config: JSON.stringify(config.stream),
+            delay: config.global.delay
+        }
+    });
+    processes[i].on('message', (m) => {
+        processEvent.msg(i, m);
+    });
+    processes[i].on('close', (code) => {
+        processEvent.exit(i, code);
+    });
+}
+
+logger("INFO", `[INFO][Thread-Main]`, `Started!`);
+
+var processEvent = {
+    msg: (i, msg) => {
+        let type = msg.type;
+
+        if (type == "request") {
+            let err = msg.data[0];
+            let code = msg.data[1];
+            total.total++;
+            n_total++;
+            if (isNaN(codeList[String(code)])) {
+                codeList[String(code)] = 1;
+            } else {
+                codeList[String(code)]++;
+            }
+            if (_code(code) && !err) {
+                n_success++;
+                total.success++
+            } else if (code != null) {
+                n_fail++;
+                total.fail++
+                logger("WARN", `[WARN][Thread-${i}]`, `Code: ${code}`);
+            }
+        } else if (type == "console") {
+            logger("INFO", `[INFO][Thread-${i}]`, msg.data);
+        }
+    },
+    exit: (i, code) => {
+        if (code == 0) {
+            logger("WARN", `[WARN][Thread-${i}]`, `exit ${code}`);
+        } else {
+            logger("ERROR", `[ERROR][Thread-${i}]`, `exit ${code}`);
+        }
+        if (restart) {
+            processes[i] = child_process.fork('./start.js');
+        }
+    }
+}
+
 process.on('SIGINT', function () {
     processes.forEach(e => {
         e.send(["exit"]);
@@ -132,5 +123,6 @@ process.on('SIGINT', function () {
             logger("INFO", `[INFO][Thread-Main]`, `${e}: ${codeList[e]}`);
         }
     })
+    logger("INFO", `[INFO][Thread-Main]`, `Powered By 黑与白工作室`);
     process.exit(0);
 });
--- start.js
+++ start.js
@@ -1,56 +1,95 @@
 const http = require("./http");
 const random = require("string-random");
-const url = process.env.url;
-const method = process.env.method;
-const t = process.env.t;
-const referer = process.env.referer;
-const data = process.env.data;
+const config = JSON.parse(process.env.config);
+const delay = process.env.delay;
 
-process.on('message',(msg) => {
-    if(msg[0] == "exit"){
+const success = (data,regexp) => {
+    let reg = new RegExp(regexp);
+    if (reg.test(data)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const funs = (str) => {
+    str = str.replace('[Phone]', random(11, { letters: false }))
+    str = str.replace('[QQ]', random(10, { letters: false }))
+    str = str.replace('[String_6]', random(6));
+    str = str.replace('[String_10]', random(10));
+    str = str.replace('[String_12]', random(12));
+    str = str.replace('[String_16]', random(16));
+    str = str.replace('[String_20]', random(20));
+    return str;
+}
+
+process.on('message', (msg) => {
+    if (msg[0] == "exit") {
         clearInterval(timer);
         process.exit(0);
     }
 })
 
 let timer = setInterval(() => {
-    if(method == "GET"){
-        uri = url.replace('[Phone]',random(11, {letters: false}))
-        uri = uri.replace('[QQ]',random(10, {letters: false}))
-        uri = uri.replace('[String_10]',random(12));
-        uri = uri.replace('[String_12]',random(12));
-        uri = uri.replace('[String_16]',random(12));
-        uri = uri.replace('[String_20]',random(20));
-        http.get(uri,referer,(err,res,body) => {
-            try {
-                let code = res.statusCode;
-                if(err){
-                    process.send([err,null,null]);
-                }else{
-                    process.send([err,code,body]);
+    config.forEach(e => {
+        let method = e.method;
+        let url = e.url;
+        let referer = e.referer;
+        let data = e.data;
+
+        if (method == "GET") {
+            let uri = funs(url);
+            http.get(uri, referer, (err, res, body) => {
+                try {
+                    let code = res.statusCode;
+                    if (err) {
+                        process.send({
+                            type: "request",
+                            data: [err, null]
+                        });
+                    } else {
+                        if(success(body,config.check)){
+                            process.send({
+                                type: "request",
+                                data: [err, code]
+                            });
+                        }else{
+                            process.send({
+                                type: "request",
+                                data: [new Error("fail"), code]
+                            });
+                        }
+                    }
+                } catch (error) {
+                    process.send({
+                        type: "request",
+                        data: [err, null, null]
+                    });
                 }
-            } catch (error) {
-                process.send([error,null,null]);
-            }
-        })
-    }else if(method == "POST"){
-        dat = data.replace('[Phone]',random(11, {letters: false}))
-        dat = dat.replace('[QQ]',random(10, {letters: false}))
-        dat = dat.replace('[String_10]',random(12));
-        dat = dat.replace('[String_12]',random(12));
-        dat = dat.replace('[String_16]',random(12));
-        dat = dat.replace('[String_20]',random(20));
-        http.post(url,referer,dat,(err,res,body) => {
-            try {
-                let code = res.statusCode;
-                if(err){
-                    process.send([err,null,null]);
-                }else{
-                    process.send([err,code,body]);
+            })
+        } else if (method == "POST") {
+            let dat = funs(data);
+            http.post(url, referer, dat, (err, res, body) => {
+                try {
+                    let code = res.statusCode;
+                    if (err) {
+                        process.send({
+                            type: "request",
+                            data: [err, null]
+                        });
+                    } else {
+                        process.send({
+                            type: "request",
+                            data: [err, code]
+                        });
+                    }
+                } catch (error) {
+                    process.send({
+                        type: "request",
+                        data: [err, null]
+                    });
                 }
-            } catch (error) {
-                process.send([error,null,null]);
-            }
-        })
-    }
-},t)
+            })
+        }
+    });
+}, delay)
